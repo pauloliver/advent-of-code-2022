06429909f2935ad3803f075eda1220862a905adc
Paul Heimburg <pauloh22@gmail.com>
2022-12-07 13:14:33 -0800

    day 6

---
+++ day6/part2.js
@@ -0,0 +1,27 @@
+const readFile = require('../lib/read-file');
+
+const input = readFile('./input')[4].split('');
+
+const isAllDifferent = (arr) => {
+    const set = new Set(arr);
+    return set.size === arr.length;
+};
+
+const getStart = (data) => {
+    const check = data.slice(0,14);
+
+    for (let i = 14; i < data.length; i++) {
+        if (isAllDifferent(check)) {
+            console.log({check: check.join(), i});
+            return i;
+        }
+
+        console.log(`old: ${check.join()}`)
+        check.shift();
+        check.push(data[i]);
+        console.log(`new: ${check.join()}`)
+        console.log('\n');
+    }
+};
+
+console.log(getStart(input));
